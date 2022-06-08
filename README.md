a379884be8c07bdcf7dd213b260a9dfdbe00b860
Forgetfulisak <forgetful.devel@gmail.com>
2022-06-08 23:03:15 +0200

    pretty-print title and link

---
+++ README.md
@@ -0,0 +1,9 @@
+# NRK-Reader
+
+Playing around with scraping articles from nrk.no. Will list all headlines and link to the article.
+
+## Usage
+
+```
+$ go run .
+```
--- main.go
+++ main.go
@@ -10,10 +10,25 @@ import (
 	"golang.org/x/net/html"
 )
 
-type nrkArticle struct {
+type Article struct {
 	smallTitle string
 	title      string
 	pageLink   string
+}
+
+var (
+	titleColor      = color.New(color.FgWhite, color.Bold)
+	smallTitleColor = color.New(color.FgWhite)
+	linkColor       = color.New(color.FgBlue)
+)
+
+func (a *Article) Print() {
+	linkColor.Println(a.pageLink)
+	if a.smallTitle != "" {
+		smallTitleColor.Println(a.smallTitle)
+
+	}
+	titleColor.Println(a.title)
 }
 
 func nodeTypeStr(t html.NodeType) string {
@@ -47,7 +62,7 @@ func printNodeTree(root *html.Node, indent int) {
 	}
 }
 
-func parseTitleNode(node *html.Node, sb *strings.Builder) {
+func parseTitleNode(node *html.Node, article *Article) {
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
 
 		if child.Type == html.TextNode {
@@ -55,58 +70,84 @@ func parseTitleNode(node *html.Node, sb *strings.Builder) {
 			cleanedData = strings.ReplaceAll(cleanedData, "\n", "")
 			cleanedData = strings.ReplaceAll(cleanedData, "\u2013", " \u2013 ")
 			cleanedData = strings.ReplaceAll(cleanedData, "\u00ad", "")
-			sb.WriteString(cleanedData)
+			text := cleanedData
 
+			if text == "" {
+				continue
+			}
+
+			if node.Type == html.ElementNode && strings.Contains(node.Data, "small") {
+				article.smallTitle = text
+			} else {
+				article.title = text
+			}
 		}
-		parseTitleNode(child, sb)
+		parseTitleNode(child, article)
 	}
-	if node.Type == html.ElementNode && strings.Contains(node.Data, "small") {
-		sb.WriteString("\n")
-	}
-
 }
 
-func _getTitleNodes(root *html.Node, titles *[]string) {
+func parseArticle(root *html.Node, article *Article) bool {
 
 	for child := root.FirstChild; child != nil; child = child.NextSibling {
 		for _, attr := range child.Attr {
+			if attr.Key == "href" {
+				article.pageLink = attr.Val
+			}
 			if attr.Key == "class" && strings.Contains(attr.Val, "kur-room__title") {
-				sb := strings.Builder{}
-				parseTitleNode(child, &sb)
-				*titles = append(*titles, sb.String())
+				parseTitleNode(child, article)
+			}
+
+		}
+		parseArticle(child, article)
+	}
+	return true
+}
+
+func _parseArticles(root *html.Node, articles *[]Article) {
+	for child := root.FirstChild; child != nil; child = child.NextSibling {
+		for _, attr := range child.Attr {
+
+			if attr.Key == "id" && strings.Contains(attr.Val, "kur-room-id") {
+				article := Article{
+					smallTitle: "",
+					title:      "",
+					pageLink:   "",
+				}
+
+				parseArticle(child, &article)
+				if article.title != "" {
+					*articles = append(*articles, article)
+				}
 			}
 		}
-		_getTitleNodes(child, titles)
+		_parseArticles(child, articles)
 	}
 }
-func getTitleNodes(root *html.Node) []string {
-	out := make([]string, 0)
-	_getTitleNodes(root, &out)
+
+func parseArticles(root *html.Node) []Article {
+	out := make([]Article, 0)
+	_parseArticles(root, &out)
 	return out
 }
 
 func main() {
-	// logrus.Info("hei!")
+	fmt.Println("fetching...")
 	resp, err := http.Get("https://nrk.no")
 	if err != nil {
-		logrus.Error("Could not get nrk.no. err: ", err)
+		logrus.Error("Could not fetch nrk.no:", err)
 	}
 	defer resp.Body.Close()
 
-	// logrus.Info("parsing...")
-
 	root, err := html.Parse(resp.Body)
 	if err != nil {
-		logrus.Error("htmlparse: ", err)
+		logrus.Error("error parsing response:", err)
 	}
 
-	whiteBold := color.New(color.FgWhite, color.Bold)
 	// printNodeTree(root, 0)
-	titles := getTitleNodes(root)
-	for _, title := range titles {
-		// fmt.Println(title)
-		whiteBold.Println(title + "\n")
+	articles := parseArticles(root)
+	fmt.Printf("%v articles found\n\n", len(articles))
+	for _, article := range articles {
+		article.Print()
 		fmt.Scanln()
-
 	}
 }
